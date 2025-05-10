71c903a0b05a2f9e14659400657bc8dc5e90b25b
VitalyL <vetallrssn@yandex.ru>
2025-05-10 09:06:47 +0300

    Добавить @EqualsAndHashCode(of = {"email"}) в User, @EqualsAndHashCode(of = {"id"}) в Post и Image.

--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.Instant;
 
 @Data
+@EqualsAndHashCode(of = {"email"})
 public class User {
     private Long id;
     private String username;
