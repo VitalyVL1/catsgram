be32d50b8fab0f462abe8bbf512c97bbe2fd897f
VitalyL <vetallrssn@yandex.ru>
2025-05-17 13:15:15 +0300

    Добавить UserService, PostService (перенсти в них бизнес-логику из UserController, PostController)

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,85 +1,37 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.findAll();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        // проверяем выполнение необходимых условий
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Имейл должен быть указан");
-        }
-        if (isContainsEmail(user)) {
-            throw new DuplicatedDataException("Этот имейл уже используется");
-        }
-        // формируем дополнительные данные
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-        // сохраняем нового пользователя в памяти приложения
-        users.put(user.getId(), user);
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
-        // проверяем необходимые условия
-        if (newUser.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-        if (users.containsKey(newUser.getId())) {
-            User oldUser = users.remove(newUser.getId());
-            if (isContainsEmail(newUser)) {
-                users.put(oldUser.getId(), oldUser); //возвращаем пользователя обратно перед тем как выбросить исключение
-                throw new DuplicatedDataException("Этот имейл уже используется");
-            }
-            if (newUser.getPassword() != null && !newUser.getPassword().isBlank()) {
-                oldUser.setPassword(newUser.getPassword());
-            }
-            if (newUser.getEmail() != null && !newUser.getEmail().isBlank()) {
-                oldUser.setEmail(newUser.getEmail());
-            }
-            if (newUser.getUsername() != null && !newUser.getUsername().isBlank()) {
-                oldUser.setUsername(newUser.getUsername());
-            }
-            // если пользователь найден и все условия соблюдены, обновляем данные
-            users.put(oldUser.getId(), oldUser);
-            return oldUser;
-        }
-        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
-    }
-
-    // вспомогательный метод для генерации идентификатора нового поста
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
-    }
-
-    private boolean isContainsEmail(User user) {
-        //можно переписать проще, убрав map, т.к. метод equals переопределен в User только по полю email, но оставил так, вдруг в будущем equals поменяется
-        return users.values().stream()
-                .map(User::getEmail)
-                .anyMatch(user.getEmail()::equals);
+        return userService.update(newUser);
     }
 }
