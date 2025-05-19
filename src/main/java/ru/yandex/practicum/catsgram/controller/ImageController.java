883c539d0ad72e283d26c3fd55992b0be78d149b
VitalyL <vetallrssn@yandex.ru>
2025-05-19 14:31:24 +0300

    Добавить возможность скачивать фотографии

--- src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
@@ -5,6 +5,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.model.ImageData;
 import ru.yandex.practicum.catsgram.service.ImageService;
 
 import java.util.List;
@@ -25,5 +26,18 @@ public class ImageController {
                                     @RequestParam("image") List<MultipartFile> files) {
         return imageService.saveImages(postId, files);
 
+    }
+
+    @GetMapping(value = "/images/{imageId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> downloadImage(@PathVariable long imageId) {
+        ImageData imageData = imageService.getImageData(imageId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDisposition(
+                ContentDisposition.attachment()
+                        .filename(imageData.getName())
+                        .build()
+        );
+
+        return new ResponseEntity<>(imageData.getData(), headers, HttpStatus.OK);
     }
 }
