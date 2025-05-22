c9cd8817d34d8b63bbafb0e19f97dcf3005e024e
VitalyL <vetallrssn@yandex.ru>
2025-05-22 14:47:41 +0300

    добавление файла application.yaml и перенос в него части настроек приложения

--- src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.catsgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatsgramApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatsgramApplication.class, args);
